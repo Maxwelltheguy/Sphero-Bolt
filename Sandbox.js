c232240bb892beffaa191518f2d2743ff3d5ce5a
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-04-18 14:17:20 -0400

    added hello world script from wiki

--- Sandbox.js
+++ Sandbox.js
@@ -6,5 +6,19 @@
 //Program URL: https://edu.sphero.com/program/16700425/edit
 
 async function startProgram() {
-	// Write code here
+//Programmer: Maxwell Moss
+//Team Member: James Maher
+//Bolt ID:SB-F28B
+//Date: 4-16-2024
+//Program: Hello World - Sandbox
+//Program URL: https://edu.sphero.com/program/16700425/edit
+
+async function startProgram() {
+await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+}
+
 }
