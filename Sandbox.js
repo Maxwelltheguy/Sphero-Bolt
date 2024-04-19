5b3a3f66ffe22b155e1884bd6ae272968c9bef2e
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-04-19 13:46:51 -0400

    Initial Hello Square

--- Sandbox.js
+++ Sandbox.js
@@ -5,6 +5,7 @@
 //Program: Hello World - Sandbox
 //Program URL: https://edu.sphero.com/program/16700425/edit
 
+/*
 async function startProgram() {
 await speak("Hello World", true);
 	setMainLed({ r: 0, g: 0, b: 255});
@@ -12,4 +13,17 @@ await speak("Hello World", true);
 	await delay (2);
 	setSpeed(0);
 }
+*/
 
+async function startProgram() {
+
+	setMainLed({ r: 255, g: 150, b: 0 });
+	await speak("Hello Square", true);
+	await delay(1);
+	for (var _i1 = 0; _i1 < 4; _i1++) {
+		setMainLed(getRandomColor());
+		await Sound.Game.Coin.play(true);
+		await roll((getHeading() + 90), 60, 1);
+		await delay(1);
+	}
+}
