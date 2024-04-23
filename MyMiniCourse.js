e11994128d4329d33492f9a78250f8fdd423dec3
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-04-23 13:32:31 -0400

    Initial MyMiniCourse Commit

---
+++ MyMiniCourse.js
@@ -0,0 +1,10 @@
+//Programmer: Maxwell Moss
+//Team Member: James Maher
+//Bolt ID:SB-F28B
+//Date: 4-23-2024
+//Program: My Mini Course
+//Program URL: https://edu.sphero.com/program/16743357/edit
+
+async function startProgram() {
+	// Write code here
+}
