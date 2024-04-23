e7f190ca338da738f1c72ed48d8da4d08330852d
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-04-23 14:12:37 -0400

    First CheckPoint

--- MyMiniCourse.js
+++ MyMiniCourse.js
@@ -6,5 +6,13 @@
 //Program URL: https://edu.sphero.com/program/16743357/edit
 
 async function startProgram() {
-	// Write code here
+	setMainLed({ r:160, g:32, b:240 });
+	await speak("Bibbity Bobbity, These shoes are now my property!");
+	await delay(1);
+	await roll((getHeading() + 0), 80, 1);
+	await delay(1);
+	await roll((getHeading() + 90), 70, 1);
+	await delay(1);
+	await Sound.EightBit.Charging.play(true);
+
 }
