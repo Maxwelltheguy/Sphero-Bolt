c6a11365592c22e402d968c9f75af151a0a567f5
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-04-24 14:18:21 -0400

    Second checkpoint

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -1,9 +1,8 @@
-//Programmer: Maxwell Moss
-//Team Member: James Maher
-//Bolt ID:SB-F28B
-//Date: 4-24-2024
-//Program: Obstacle Course
-//Program URL: https://edu.sphero.com/program/16752554/edit
+//Programmer: James Maher
+//Team Member: Max Moss
+//Date ID: 4.16.2024
+//Program: Hello world - Sandbox
+//program url: https://edu.sphero.com/program/16752575/edit
 
 //Maxwell Moss
 async function startProgram() {
@@ -11,4 +10,8 @@ async function startProgram() {
 	await speak("Imma beat this thing")
 	await roll((getHeading() + 0), 40, 1);
 	await roll((getHeading() + 45), 80, 1);
+	//James Maher
+	await delay(1);
+	setMainLed({r:10, g:100, b:115});
+	await roll((getHeading() - 20), 100, 1);
 }
