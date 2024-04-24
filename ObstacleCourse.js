d122250d4186023ec9c617c9d86c140090c4a618
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-04-24 14:05:26 -0400

    First Checkpoint

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -4,3 +4,11 @@
 //Date: 4-24-2024
 //Program: Obstacle Course
 //Program URL: https://edu.sphero.com/program/16752554/edit
+
+//Maxwell Moss
+async function startProgram() {
+	setMainLed({ r:0, g:0, b:255});
+	await speak("Imma beat this thing")
+	await roll((getHeading() + 0), 40, 1);
+	await roll((getHeading() + 45), 80, 1);
+}
