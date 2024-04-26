08d370b7ce14f772b6d9345c48d38675f650e866
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-04-26 14:05:45 -0400

    4th checkpoint + Sounds

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -10,14 +10,20 @@ async function startProgram() {
 	await speak("Imma beat this thing")
 	await roll((getHeading() + 0), 40, 1);
 	await roll((getHeading() + 45), 80, 1);
+	await Sound.EightBit.PowerUp.play(true);
 	//James Maher
 	await delay(1);
 	setMainLed({r:10, g:100, b:115});
 	await roll((getHeading() - 20), 100, 1);
+	await Sound.ScienceFiction.PhoneHome.play(true);
 	//Maxwell Moss - PC 7 - checkpoint 3 - green
 	await delay(1);
 	setMainLed({r:0, g:225, b:0});
 	await roll((getHeading() + 125), 150, 1);
 	await Sound.Mechanical.Hammer.play(true); 
 	await delay(1);
+	//James Maher - pc 8 -checkpoint 4 - red
+	setMainLed({r:225, g:0, b:0});
+	await roll((getHeading() - 95), 180, 1);
+	await Sound.EightBit.LevelComplete.play(true); 
 }
