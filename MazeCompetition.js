982a13b2c6b6a9ff5da1ddd4c34f019e9661d116
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-04-30 13:29:12 -0400

    Added Beginning comments and start code

---
+++ MazeCompetition.js
@@ -0,0 +1,10 @@
+//Programmer: Maxwell Moss
+//Team Member: James Maher
+//Bolt ID:SB-F28B
+//Date: 4.16.2024
+//Program: Hello world - Sandbox
+//program url:https://edu.sphero.com/program/16788354/edit
+
+async function startProgram() {
+	// Write code here
+}
