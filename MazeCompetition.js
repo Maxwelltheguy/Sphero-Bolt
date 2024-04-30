2a53f67994b7a03969c60d2fa8c544b5fcefc19e
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-04-30 14:18:31 -0400

    First Checkpoint Sound And text

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -6,5 +6,39 @@
 //program url:https://edu.sphero.com/program/16788354/edit
 
 async function startProgram() {
-	// Write code here
+	// Programmer: Maxwell Moss - Start Text & Audio Checkpoint Commit - Date:
+	await scrollMatrixText('Start', { r: 255, g: 255, b: 255 }, 15, true);
+	await await speak("Start", true);
+
+
+	// Programmer:  - Blue LED Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Sound One Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Red LED Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Sound Two Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Green LED Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Sound Two Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Purple LED & Sound Three Checkpoint Commit - Date:
+
+
+
+	// Programmer: Maxwell Moss - Finish Text & Audio Checkpoint Commit - Date:	
+
 }
