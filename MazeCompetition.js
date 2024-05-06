b9e13bdbc45f9e4a2bc6dd6766cc19b0f0799e61
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-05-06 14:20:47 -0400

    5-6-2024

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -1,12 +1,12 @@
-//Programmer: Maxwell Moss
-//Team Member: James Maher
-//Bolt ID:SB-F28B
-//Date: 4.16.2024
-//Program: Hello world - Sandbox
+     //Programmer: Maxwell Moss
+    //Team Member: James Maher
+   //Bolt ID:SB-F28B
+  //Date: 4.16.2024
+ //Program: Hello world - Sandbox
 //program url:https://edu.sphero.com/program/16788354/edit
 
 	async function startProgram(){
-// Programmer: Maxwell Moss - Start Text & Audio Checkpoint Commit - Date: 4-31-2024
+	// Programmer: Maxwell Moss - Start Text & Audio Checkpoint Commit - Date: 4-31-2024
 	await scrollMatrixText('Start', { r: 255, g: 255, b: 255 }, 15, true);
 	await speak("Start", true);
 
@@ -22,12 +22,21 @@
 	await Sound.Water.BoatAngry.play(true);
 
 
-	// Programmer:  - Red LED Checkpoint Commit - Date:
+	// Programmer:James Maher  - Red LED Checkpoint Commit - Date:5-6-2024
+	await delay(1);
+	await roll((getHeading() + 90), 50, 1.5);
+	await delay(1);
+	await roll((getHeading() + 45), 50, 1.5);
+	setMainLed({r:255, g:0, b:0});
 
 
 
-	// Programmer:  - Sound Two Checkpoint Commit - Date:
-
+	// Programmer: Maxwell Moss - Sound Two Checkpoint Commit - Date:
+	await delay(1);
+	await roll((getHeading() - 90), 50, 0.5);
+	await delay(1);
+	await roll((getHeading() - 90), 50, 1.5);
+	await Sound.Water.BoatAngry.play(true);
 
 
 	// Programmer:  - Green LED Checkpoint Commit - Date:
